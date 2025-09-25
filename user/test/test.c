efe623fc5355848fdeed6d32efd5ee9e537d853e
pulitha13 <joshuagm12@gmail.com>
2025-09-25 18:10:31 -0400

    adding rand weights and "balance analysis"

--- user/test/test.c
+++ user/test/test.c
@@ -8,7 +8,8 @@
 #include <string.h>
 #include <signal.h>
 #include <getopt.h>
-#include <sched.h>
+#include <math.h>
+#include <time.h>
 
 #define __NR_get_wfq_info 467
 #define __NR_set_wfq_weight 468
@@ -63,19 +64,19 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    srand(time(NULL));
     printf("Starting %d test processes for %d seconds...\n", num_procs, timeout);
 
     for (int i = 0; i < num_procs; i++) {
+        int weight = (rand() % 50) + 1;
         pid_t pid = fork();
         if (pid == 0) {
             // Child process: assign a weight (heavier every 3rd process)
-            int weight = (i % 3 == 0) ? 20 : 10;
             if (set_wfq_weight(weight) < 0) {
                 fprintf(stderr, "Child %d: set_wfq_weight(%d) failed: %s\n",
                         getpid(), weight, strerror(errno));
-            } else {
-                printf("Child %d: weight set to %d\n", getpid(), weight);
             }
+	    fprintf(stderr, "Child %d: set_wfq_weight(%d)\n",getpid(), weight);
             busy_work();
             exit(0);
         } else if (pid > 0) {
@@ -86,26 +87,63 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Parent: sample scheduler state once per second
+    // Store samples for statistics
+    int samples = 0;
+    double cpu_weight_sums[MAX_CPUS] = {0};
+    double cpu_weight_sq_sums[MAX_CPUS] = {0};
+    double imbalance_sum = 0.0;
+
     for (int t = 0; t < timeout; t++) {
         struct wfq_info info;
         if (get_wfq_info(&info) < 0) {
             perror("get_wfq_info");
             break;
         }
-        printf("[t=%d] CPU distribution:\n", t);
+
+        int min_w = info.total_weight[0];
+        int max_w = info.total_weight[0];
+
         for (int c = 0; c < info.num_cpus; c++) {
-            printf("  CPU %d: nr_running=%d total_weight=%d\n",
-                   c, info.nr_running[c], info.total_weight[c]);
+            int w = info.total_weight[c];
+            cpu_weight_sums[c] += w;
+            cpu_weight_sq_sums[c] += (double)w * (double)w;
+            if (w < min_w) min_w = w;
+            if (w > max_w) max_w = w;
         }
+
+        imbalance_sum += (max_w - min_w);
+        samples++;
         sleep(1);
     }
+
+    printf("Killing children...\n");
 
     // Cleanup children
     for (int i = 0; i < num_procs; i++) {
         kill(pids[i], SIGKILL);
         waitpid(pids[i], NULL, 0);
-        printf("Child %d: killed\n", pids[i]);
+    }
+
+    // Print summary
+    if (samples > 0) {
+        printf("\n=== WFQ Scheduler Balance Analysis ===\n");
+        printf("Collected %d samples over %d seconds\n\n", samples, timeout);
+
+        printf("CPU   AvgWeight   StdevWeight\n");
+        printf("------------------------------\n");
+        for (int c = 0; c < MAX_CPUS; c++) {
+            if (cpu_weight_sums[c] > 0) {
+                double mean = cpu_weight_sums[c] / samples;
+                double mean_sq = cpu_weight_sq_sums[c] / samples;
+                double variance = mean_sq - (mean * mean);
+                if (variance < 0) variance = 0; // numerical safety
+                double stdev = sqrt(variance);
+                printf("%2d    %.2f       %.2f\n", c, mean, stdev);
+            }
+        }
+
+        printf("\nAverage per-sample imbalance (max-min): %.2f\n",
+               imbalance_sum / samples);
     }
 
     free(pids);
