0386e72c309e664e723dc645f49a652d67e2a3a8
pulitha13 <joshuagm12@gmail.com>
2025-09-26 19:43:40 -0400

    cleaning up test exec

--- user/test/test.c
+++ user/test/test.c
@@ -2,18 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sched.h>
+#include <string.h>
 #include <sys/syscall.h>
 #include <sys/wait.h>
 #include <errno.h>
-#include <string.h>
 #include <signal.h>
 #include <getopt.h>
 #include <math.h>
 #include <time.h>
+#include <fcntl.h>
 
 #define __NR_get_wfq_info 467
 #define __NR_set_wfq_weight 468
+#define SCHED_WFS 8
 #define MAX_CPUS 8
+
+pid_t *pids;
+int num_procs;
 
 struct wfq_info {
     int num_cpus;
@@ -29,7 +35,7 @@ static inline int set_wfq_weight(int weight) {
     return syscall(__NR_set_wfq_weight, weight);
 }
 
-void busy_work(void) {
+void busy_cpu(void) {
     volatile unsigned long long x = 0;
     while (1) {
         x += 1;
@@ -39,45 +45,44 @@ void busy_work(void) {
     }
 }
 
-int main(int argc, char *argv[]) {
-    int num_procs = 4;   // default
-    int timeout = 20;    // default seconds
-
-    int opt;
-    while ((opt = getopt(argc, argv, "n:t:")) != -1) {
-        switch (opt) {
-        case 'n':
-            num_procs = atoi(optarg);
-            break;
-        case 't':
-            timeout = atoi(optarg);
-            break;
-        default:
-            fprintf(stderr, "Usage: %s [-n num_procs] [-t seconds]\n", argv[0]);
-            exit(EXIT_FAILURE);
+void busy_io(void) {
+    char buf[4096];
+    memset(buf, 'A', sizeof(buf));
+    while (1) {
+        int fd = open("/tmp/busyio.tmp", O_RDWR | O_CREAT, 0644);
+        if (fd < 0) {
+            perror("open");
+            continue;
         }
+        ssize_t ret = write(fd, buf, sizeof(buf));
+	(void)ret;	
+        lseek(fd, 0, SEEK_SET);
+        ret = read(fd, buf, sizeof(buf));
+	(void)ret;	
+        close(fd);
     }
+}
 
-    pid_t *pids = calloc(num_procs, sizeof(pid_t));
-    if (!pids) {
-        perror("calloc");
-        exit(1);
-    }
+int spawn_children(int num_children, int use_rand, int use_cpu) {
 
-    srand(time(NULL));
-    printf("Starting %d test processes for %d seconds...\n", num_procs, timeout);
+    printf("Starting %d test processes...\n", num_children);
 
-    for (int i = 0; i < num_procs; i++) {
-        int weight = (rand() % 50) + 1;
+    for (int i = 0; i < num_children; i++) {
+        int weight = use_rand ? ((rand() % 10) + 1) : 5;
+
         pid_t pid = fork();
+	
         if (pid == 0) {
-            // Child process: assign a weight (heavier every 3rd process)
+	    
             if (set_wfq_weight(weight) < 0) {
                 fprintf(stderr, "Child %d: set_wfq_weight(%d) failed: %s\n",
                         getpid(), weight, strerror(errno));
             }
-	    fprintf(stderr, "Child %d: set_wfq_weight(%d)\n",getpid(), weight);
-            busy_work();
+            if (use_cpu) {
+                busy_cpu();
+            } else {
+                busy_io();
+            }
             exit(0);
         } else if (pid > 0) {
             pids[i] = pid;
@@ -87,11 +92,17 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // Store samples for statistics
-    int samples = 0;
-    double cpu_weight_sums[MAX_CPUS] = {0};
-    double cpu_weight_sq_sums[MAX_CPUS] = {0};
-    double imbalance_sum = 0.0;
+    return 0;
+}
+
+
+int monitor_and_print_stats(int timeout, int verbose) {
+
+    // Print header
+    if (!verbose) {
+        printf("\nTime  L2_Distance\n");
+        printf("------------------\n");
+    }
 
     for (int t = 0; t < timeout; t++) {
         struct wfq_info info;
@@ -100,51 +111,109 @@ int main(int argc, char *argv[]) {
             break;
         }
 
-        int min_w = info.total_weight[0];
-        int max_w = info.total_weight[0];
-
+        int total = 0;
         for (int c = 0; c < info.num_cpus; c++) {
-            int w = info.total_weight[c];
-            cpu_weight_sums[c] += w;
-            cpu_weight_sq_sums[c] += (double)w * (double)w;
-            if (w < min_w) min_w = w;
-            if (w > max_w) max_w = w;
+            total += info.total_weight[c];
+        }
+        double ideal = (info.num_cpus > 0) ? ((double) total / info.num_cpus) : 0.0;
+
+        double dist_sq = 0.0;
+        for (int c = 0; c < info.num_cpus; c++) {
+            double diff = (double)info.total_weight[c] - ideal;
+            dist_sq += diff * diff;
+        }
+        double l2 = sqrt(dist_sq);
+
+        if (verbose) {
+            printf("Time %d sec:\n", t);
+            printf("  L2 distance: %.2f\n", l2);
+            printf("  CPU weights: ");
+            for (int c = 0; c < info.num_cpus; c++) {
+                printf("%d ", info.total_weight[c]);
+            }
+	    printf("\n");
+            fflush(stdout);
+        } else {
+            printf("%4d  %.2f\n", t, l2);
+            fflush(stdout);
         }
 
-        imbalance_sum += (max_w - min_w);
-        samples++;
         sleep(1);
     }
 
     printf("Killing children...\n");
 
-    // Cleanup children
     for (int i = 0; i < num_procs; i++) {
         kill(pids[i], SIGKILL);
         waitpid(pids[i], NULL, 0);
     }
 
-    // Print summary
-    if (samples > 0) {
-        printf("\n=== WFQ Scheduler Balance Analysis ===\n");
-        printf("Collected %d samples over %d seconds\n\n", samples, timeout);
+    printf("Done killing...\n");
 
-        printf("CPU   AvgWeight   StdevWeight\n");
-        printf("------------------------------\n");
-        for (int c = 0; c < MAX_CPUS; c++) {
-            if (cpu_weight_sums[c] > 0) {
-                double mean = cpu_weight_sums[c] / samples;
-                double mean_sq = cpu_weight_sq_sums[c] / samples;
-                double variance = mean_sq - (mean * mean);
-                if (variance < 0) variance = 0; // numerical safety
-                double stdev = sqrt(variance);
-                printf("%2d    %.2f       %.2f\n", c, mean, stdev);
-            }
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    num_procs = 16;   // default
+    int timeout   = 10;   // default seconds
+    int use_rand  = 0;    // default no rand
+    int verbose   = 1;    // default verbose
+    int test   = 2;    // default both
+
+    int opt;
+    while ((opt = getopt(argc, argv, "n:t:rvci")) != -1) {
+        switch (opt) {
+        case 'n':
+            num_procs = atoi(optarg);
+            break;
+        case 't':
+            timeout = atoi(optarg);
+            break;
+        case 'r':
+            use_rand = 1;
+            break;
+        case 'q':
+            verbose = 0;
+            break;
+        case 'c':
+            test = 1;
+            break;
+        case 'i':
+            test = 0;
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [-n num_procs] [-t seconds] [-r] [-v] [-c|-i]\n", argv[0]);
+            exit(EXIT_FAILURE);
         }
-
-        printf("\nAverage per-sample imbalance (max-min): %.2f\n",
-               imbalance_sum / samples);
     }
+
+    // Elevate parent to RT scheduling
+
+    pids = calloc(num_procs, sizeof(pid_t));
+    if (!pids) {
+        perror("calloc");
+        exit(1);
+    }
+
+    srand(time(NULL));
+
+    if (test == 2) {
+	    printf("Executing CPU stress test\n");
+	    spawn_children(num_procs, use_rand, 1);
+	    monitor_and_print_stats(timeout, verbose);
+	    printf("Executing IO stress test\n");
+	    spawn_children(num_procs, use_rand, 0);
+	    monitor_and_print_stats(timeout, verbose);
+    } else if (test == 1) {
+	    printf("Executing CPU stress test\n");
+	    spawn_children(num_procs, use_rand, 1);
+	    monitor_and_print_stats(timeout, verbose);
+    } else {
+	    printf("Executing IO stress test\n");
+	    spawn_children(num_procs, use_rand, 0);
+	    monitor_and_print_stats(timeout, verbose);
+    }
+
 
     free(pids);
     return 0;
